b20b6ec3533221ce608b66712c1c06c3bc058bd7
SnowPhoenix <1595763343@qq.com>
2020-10-18 16:41:39 +0800

    reverse

--- Develop/AbstractSyntacticAnalyzeTactics.h
+++ Develop/AbstractSyntacticAnalyzeTactics.h
@@ -185,136 +185,227 @@ protected:
 	*/
 };
 
-// 程序
-struct ProgramAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set = 
-	{ 
-		SymbolType::key_const, SymbolType::key_int, SymbolType::key_char, SymbolType::key_void
-	};
-protected:
-	void analyze(Env& env);
-};
-
-// 常量说明
-struct ConstantDeclarationAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =  
-	{
-		SymbolType::key_const
-	};
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 常量定义
-struct ConstantDefinationAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set = 
-	{ 
-		SymbolType::key_int, SymbolType::key_char
-	};
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 变量说明
-struct VariableDeclarationAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set = 
-	{ 
-		SymbolType::key_int, SymbolType::key_char
-	};
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 变量定义
-struct VariableDefinationAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set = 
-	{
-		SymbolType::key_int, SymbolType::key_char
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 变量定义及初始化
-struct VariableDefinationWithInitializationAnalyze : AbstractSyntacticAnalyzeTactics
+// 因子
+struct FactorAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_int, SymbolType::key_char
+		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
+		SymbolType::left_paren,				// (表达式)
+		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
+		SymbolType::character				// 字符
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 变量定义无初始化
-struct VariableDefinationNoInitializationAnalyze : AbstractSyntacticAnalyzeTactics
+// 项
+struct TermAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_int, SymbolType::key_char
+		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
+		SymbolType::left_paren,				// (表达式)
+		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
+		SymbolType::character				// 字符
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 常量
-struct ConstantAnalyze : AbstractSyntacticAnalyzeTactics
+// 表达式
+struct ExpressionAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::character, SymbolType::plus, SymbolType::minus, SymbolType::number
+		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
+		SymbolType::left_paren,				// (表达式)
+		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
+		SymbolType::character				// 字符
 	};
-	bool is_type_of(BaseType type);
-	char get_char()
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 返回语句
+struct ReturnStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		return ch;
+		SymbolType::key_return
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 缺省
+struct DefaultCaseAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_default
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 情况子语句
+struct CaseStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_case
+	};
+	int get_case_value()
+	{
+		return case_value;
 	}
-	int get_int()
+protected:
+	virtual void analyze(Env& env);
+	int case_value;
+};
+
+// 情况表
+struct SwitchTableAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		return integer;
-	}
+		SymbolType::key_case
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 情况语句
+struct SwitchStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_switch
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 写语句
+struct WriteStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_printf
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 读语句
+struct ReadStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_scanf
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 赋值语句
+struct AssignmentStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::identifier
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 值参数表
+struct ParameterValueListAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
+		SymbolType::left_paren,				// (表达式)
+		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
+		SymbolType::character				// 字符
+	};
+	ParameterValueListAnalyze(shared_ptr<const vector<BaseType>> param_type_list)
+		:param_type_list(param_type_list) { }
 protected:
 	virtual void analyze(Env& env);
 private:
-	bool is_ch;
-	char ch;
-	int integer;
+	shared_ptr<const vector<BaseType>> param_type_list;
 };
 
-// 整数
-struct IntegerAnalyze : AbstractSyntacticAnalyzeTactics
+// 无返回值函数调用语句
+struct CallVoidFunctionStatementAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::plus, SymbolType::minus, SymbolType::number
+		SymbolType::identifier
 	};
 
-	int get_value()
-	{
-		return value;
-	}
 protected:
 	virtual void analyze(Env& env);
-private:
-	int value;
 };
 
-// 无符号整数
-struct UnsignedIntegerAnalyze : AbstractSyntacticAnalyzeTactics
+// 有返回值函数调用语句
+struct CallReturnFunctionStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::identifier
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 条件
+struct ConditionAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
+		SymbolType::left_paren,				// (表达式)
+		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
+		SymbolType::character				// 字符
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 条件语句
+struct ConditionStatementAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_if
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 步长
+struct StepLengthAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
 		SymbolType::number
 	};
-
 	unsigned get_value()
 	{
 		return value;
@@ -325,90 +416,23 @@ private:
 	unsigned value;
 };
 
-// 无返回值函数定义
-struct VoidFunctionDefinationAnalyze : AbstractSyntacticAnalyzeTactics
+// 循环语句
+struct LoopStatementAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_void
+		SymbolType::key_while, SymbolType::key_for
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 有返回值函数定义
-struct ReturnFunctionDefinationAnalyze : AbstractSyntacticAnalyzeTactics
+// 语句
+struct StatementAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_int, SymbolType::key_char
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 主函数
-struct MainFunctionAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_void
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 声明头部
-struct FunctionHeaderAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_char, SymbolType::key_int
-	};
-	shared_ptr<const string> get_id()
-	{
-		return id;
-	}
-	// 只能是 type_int / type_char
-	BaseType get_return_type()
-	{
-		return return_type;
-	}
-protected:
-	virtual void analyze(Env& env);
-private:
-	shared_ptr<const string> id;
-	BaseType return_type;
-};
-
-// 参数表
-struct ParameterListAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_char, SymbolType::key_int
-	};
-	shared_ptr<const vector<BaseType>> get_param_type_list();
-	shared_ptr<const vector<shared_ptr<IdentifierInfo>>> get_param_list()
-	{
-		return param_list;
-	}
-protected:
-	virtual void analyze(Env& env);
-private:
-	shared_ptr<vector<shared_ptr<IdentifierInfo>>> param_list;
-};
-
-// 复合语句
-struct CompoundStatementsAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_int, SymbolType::key_char,	// 常量/变量说明
-		// 语句列
 		SymbolType::key_while, SymbolType::key_for, // 循环语句
 		SymbolType::key_if,							// 条件语句
 		SymbolType::identifier,						// 有/无返回值函数调用/赋值语句
@@ -444,11 +468,13 @@ protected:
 	virtual void analyze(Env& env);
 };
 
-// 语句
-struct StatementAnalyze : AbstractSyntacticAnalyzeTactics
+// 复合语句
+struct CompoundStatementsAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
+		SymbolType::key_int, SymbolType::key_char,	// 常量/变量说明
+		// 语句列
 		SymbolType::key_while, SymbolType::key_for, // 循环语句
 		SymbolType::key_if,							// 条件语句
 		SymbolType::identifier,						// 有/无返回值函数调用/赋值语句
@@ -464,25 +490,91 @@ protected:
 	virtual void analyze(Env& env);
 };
 
-// 循环语句
-struct LoopStatementAnalyze : AbstractSyntacticAnalyzeTactics
+// 参数表
+struct ParameterListAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_while, SymbolType::key_for
+		SymbolType::key_char, SymbolType::key_int
+	};
+	shared_ptr<const vector<BaseType>> get_param_type_list();
+	shared_ptr<const vector<shared_ptr<IdentifierInfo>>> get_param_list()
+	{
+		return param_list;
+	}
+protected:
+	virtual void analyze(Env& env);
+private:
+	shared_ptr<vector<shared_ptr<IdentifierInfo>>> param_list;
+};
+
+// 声明头部
+struct FunctionHeaderAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_char, SymbolType::key_int
+	};
+	shared_ptr<const string> get_id()
+	{
+		return id;
+	}
+	// 只能是 type_int / type_char
+	BaseType get_return_type()
+	{
+		return return_type;
+	}
+protected:
+	virtual void analyze(Env& env);
+private:
+	shared_ptr<const string> id;
+	BaseType return_type;
+};
+
+// 主函数
+struct MainFunctionAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_void
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 步长
-struct StepLengthAnalyze: AbstractSyntacticAnalyzeTactics
+// 有返回值函数定义
+struct ReturnFunctionDefinationAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_int, SymbolType::key_char
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 无返回值函数定义
+struct VoidFunctionDefinationAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_void
+	};
+
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 无符号整数
+struct UnsignedIntegerAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
 		SymbolType::number
 	};
+
 	unsigned get_value()
 	{
 		return value;
@@ -493,218 +585,126 @@ private:
 	unsigned value;
 };
 
-// 条件语句
-struct ConditionStatementAnalyze: AbstractSyntacticAnalyzeTactics
+// 整数
+struct IntegerAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_if
+		SymbolType::plus, SymbolType::minus, SymbolType::number
 	};
 
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 条件
-struct ConditionAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
+	int get_value()
 	{
-		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
-		SymbolType::left_paren,				// (表达式)
-		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
-		SymbolType::character				// 字符
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 有返回值函数调用语句
-struct CallReturnFunctionStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::identifier
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 无返回值函数调用语句
-struct CallVoidFunctionStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::identifier
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 值参数表
-struct ParameterValueListAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
-		SymbolType::left_paren,				// (表达式)
-		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
-		SymbolType::character				// 字符
-	};
-	ParameterValueListAnalyze(shared_ptr<const vector<BaseType>> param_type_list)
-		:param_type_list(param_type_list) { }
-protected:
-	virtual void analyze(Env& env);
-private:
-	shared_ptr<const vector<BaseType>> param_type_list;
-};
-
-// 赋值语句
-struct AssignmentStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::identifier
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 读语句
-struct ReadStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_scanf
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 写语句
-struct WriteStatementAnalyze: AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_printf
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 情况语句
-struct SwitchStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_switch
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 情况表
-struct SwitchTableAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_case
-	};
-
-protected:
-	virtual void analyze(Env& env);
-};
-
-// 情况子语句
-struct CaseStatementAnalyze : AbstractSyntacticAnalyzeTactics
-{
-	static constexpr std::initializer_list<SymbolType> first_set =
-	{
-		SymbolType::key_case
-	};
-	int get_case_value()
-	{
-		return case_value;
+		return value;
 	}
 protected:
 	virtual void analyze(Env& env);
-	int case_value;
+private:
+	int value;
 };
 
-// 缺省
-struct DefaultCaseAnalyze: AbstractSyntacticAnalyzeTactics
+// 常量
+struct ConstantAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_default
+		SymbolType::character, SymbolType::plus, SymbolType::minus, SymbolType::number
+	};
+	bool is_type_of(BaseType type);
+	char get_char()
+	{
+		return ch;
+	}
+	int get_int()
+	{
+		return integer;
+	}
+protected:
+	virtual void analyze(Env& env);
+private:
+	bool is_ch;
+	char ch;
+	int integer;
+};
+
+// 变量定义无初始化
+struct VariableDefinationNoInitializationAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_int, SymbolType::key_char
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 返回语句
-struct ReturnStatementAnalyze : AbstractSyntacticAnalyzeTactics
+// 变量定义及初始化
+struct VariableDefinationWithInitializationAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::key_return
+		SymbolType::key_int, SymbolType::key_char
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 表达式
-struct ExpressionAnalyze : AbstractSyntacticAnalyzeTactics
+// 变量定义
+struct VariableDefinationAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
-		SymbolType::left_paren,				// (表达式)
-		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
-		SymbolType::character				// 字符
+		SymbolType::key_int, SymbolType::key_char
 	};
 
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 项
-struct TermAnalyze : AbstractSyntacticAnalyzeTactics
+// 变量说明
+struct VariableDeclarationAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
-		SymbolType::left_paren,				// (表达式)
-		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
-		SymbolType::character				// 字符
+		SymbolType::key_int, SymbolType::key_char
 	};
-
 protected:
 	virtual void analyze(Env& env);
 };
 
-// 因子
-struct FactorAnalyze : AbstractSyntacticAnalyzeTactics
+// 常量定义
+struct ConstantDefinationAnalyze : AbstractSyntacticAnalyzeTactics
 {
 	static constexpr std::initializer_list<SymbolType> first_set =
 	{
-		SymbolType::identifier,				// 标识符/一维数组/二维数组/有返回值函数调用
-		SymbolType::left_paren,				// (表达式)
-		SymbolType::plus, SymbolType::minus, SymbolType::number,	//整数
-		SymbolType::character				// 字符
+		SymbolType::key_int, SymbolType::key_char
 	};
-
 protected:
 	virtual void analyze(Env& env);
+};
+
+// 常量说明
+struct ConstantDeclarationAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set =
+	{
+		SymbolType::key_const
+	};
+protected:
+	virtual void analyze(Env& env);
+};
+
+// 程序
+struct ProgramAnalyze : AbstractSyntacticAnalyzeTactics
+{
+	static constexpr std::initializer_list<SymbolType> first_set = 
+	{ 
+		SymbolType::key_const, SymbolType::key_int, SymbolType::key_char, SymbolType::key_void
+	};
+protected:
+	void analyze(Env& env);
 };
 
 /*
