dda8c14d9d69eac9a2099cd671f9c78d41cc8cd2
SnowPhoenix <1595763343@qq.com>
2020-11-16 11:25:24 +0800

    AC

--- UnitTest/TestResource/blackbox_simple_test/sample3/ir.asm
+++ UnitTest/TestResource/blackbox_simple_test/sample3/ir.asm
@@ -22,5 +22,18 @@ func_mid_main:
 	main_tmp_13	=	main_var_a1	*	1
 	main_tmp_14	=	$0	+	main_tmp_13
 	printf	NIL,	main_tmp_14,	int
+	main_tmp_16	=	4	*	1
+	main_tmp_17	=	$0	+	main_tmp_16
+	main_tmp_15	=	main_tmp_17	+	$0
+	main_var_a1	=	main_tmp_15	+	$0
+	main_tmp_19	=	-1	*	1
+	main_tmp_20	=	97	*	main_tmp_19
+	main_tmp_21	=	main_var_a1	*	main_tmp_20
+	main_tmp_22	=	$0	+	main_tmp_21
+	main_tmp_18	=	main_tmp_22	+	$0
+	main_var_a1	=	main_tmp_18	+	$0
+	main_tmp_23	=	main_var_a1	*	1
+	main_tmp_24	=	$0	+	main_tmp_23
+	printf	NIL,	main_tmp_24,	int
 func_end_main:
 
