80e213384b89221d55f5396dcdfdb658a42de10c
SnowPhoenix <1595763343@qq.com>
2020-12-10 19:40:53 +0800

    init calculate-strength-reduction

---
+++ Develop/OptimizerCalculateStrengthReduction.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef __OPTIMIZER_CALCULATE_STRENGTH_REDUCTION_H__
+#define __OPTIMIZER_CALCULATE_STRENGTH_REDUCTION_H__
+
+#include "IOptimizer.h"
+class OptimizerCalculateStrengthReduction :
+    public IOptimizer
+{
+};
+
+#endif // !__OPTIMIZER_CALCULATE_STRENGTH_REDUCTION_H__
+
+
